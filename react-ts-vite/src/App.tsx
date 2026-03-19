678afcf6f610d974db63a578e3892ba4fd987873
UNCAlfonsoTipan <alfonso_tipan@unicomer.com>
2026-03-18 23:12:25 -0500

    actualizado

---
+++ react-ts-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>React + TypeScript + Vite</h1>
+      <p>Plantilla lista para empezar a desarrollar.</p>
+    </>
+  )
+}
+
+export default App
